e7d7ead8c20a07d44bace3f641cdaf3e12c5dc68
JustineTang10 <68197279+JustineTang10@users.noreply.github.com>
2026-03-01 12:22:21 -0500

    Update ilp.py

--- ilp.py
+++ ilp.py
@@ -1,4 +1,5 @@
-from pulp import *
+import highspy
+import numpy as np
 
 class ILP:
     def __init__(self, r, m, a):
@@ -14,39 +15,32 @@ class ILP:
             self.f = max(self.f, cur_f)
     
     def solve(self):
-        prob = LpProblem("rule_evaluation_problem", LpMinimize)
-        x_names = list(range(self.m))
-        x = LpVariable.dicts('x', x_names, lowBound=0, upBound=1)
-        prob += (
-            lpSum([x[i] for i in x_names]),
-            'Number of roots',
-        )
+        h = highspy.Highs()
+        h.setOptionValue("output_flag", False)
+
+        for i in range(self.m):
+            h.addVariable(0, 1)
+            h.changeColCost(i, 1)
+
+        inf = highspy.kHighsInf
         for i in range(self.r):
-            prob += (
-                lpSum([self.a[i][j] * x[j] for j in x_names]) >= 1,
-                f'ConstraintForRule{i}',
-            )
-            
-        prob.writeLP('RuleEval.lp')
-        prob.solve()
+            nz_indices = []
+            nz_values = []
+            for j in range(self.m):
+                if self.a[i][j] != 0:
+                    nz_indices.append(j)
+                    nz_values.append(1)
+            num_nz = sum(self.a[i])
+            h.addRow(1, inf, num_nz, np.array(nz_indices), np.array(nz_values))
         
+        h.run()
+        solution = h.getSolution()
+
         ans = []
-        for v in prob.variables():
-            if v.varValue >= 1 / self.f:
+        for v in solution.col_value:
+            if v >= 1 / self.f:
                 ans.append(1)
             else:
                 ans.append(0)
-        
-        for i in range(self.r):
-            is_covered = False
-            for j in range(self.m):
-                if self.a[i][j] * ans[j] == 1:
-                    is_covered = True
-            if not is_covered:
-                min_k = -1
-                for j in range(self.m):
-                    if self.a[i][j]:
-                        min_k = j
-                ans[min_k] = 1
         
         return ans
